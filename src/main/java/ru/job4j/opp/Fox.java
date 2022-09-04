45135d161ad445e966ec868850ef3642fc566e6f
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-09-04 14:10:55 +0500

    1.6. Взаимодействие объектов [#166087 #339374]

--- src/main/java/ru/job4j/opp/Fox.java
+++ src/main/java/ru/job4j/opp/Fox.java
@@ -2,7 +2,7 @@ package ru.job4j.opp;
 
 public class Fox {
 
-    public void tryEat(Ball ball){
+    public void tryEat(Ball ball) {
         ball.tryRun(true);
     }
 }
--- src/main/java/ru/job4j/opp/Hare.java
+++ src/main/java/ru/job4j/opp/Hare.java
@@ -2,7 +2,7 @@ package ru.job4j.opp;
 
 public class Hare {
 
-    public void tryEat(Ball ball){
+    public void tryEat(Ball ball) {
         ball.tryRun(false);
     }
 }
