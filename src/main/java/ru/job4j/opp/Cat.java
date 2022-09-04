2d056f10deedc29aecba049feda5ab958fb96abb
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-09-04 12:05:33 +0500

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/opp/Cat.java
+++ src/main/java/ru/job4j/opp/Cat.java
@@ -2,15 +2,40 @@ package ru.job4j.opp;
 
 public class Cat {
 
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.printf("The cat %s ate %s%n", this.name, this.food);
+    }
+
+    void eat(String meat) {
+        this.food = meat;
+    }
+
+    void giveName(String name) {
+        this.name = name;
+    }
+
     public static void main(String[] args) {
-        Cat petty = new Cat();
-        Cat sparky = new Cat();
-        String say = petty.sound();
-        System.out.println("Petty say: " + say);
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveName("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveName("Black");
+        black.eat("fish");
+        black.show();
+        System.out.println("There are nick's food.");
+        Cat nick = new Cat();
+        nick.giveName("Nick");
+        nick.eat("bird");
+        nick.show();
     }
 
     public String sound() {
-        String voice = "may-may";
-        return voice;
+        return "may-may";
     }
 }
