eb9d6b220f9357de2ccb9a9e004360633134a2cd
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-09-04 11:13:22 +0500

    1.2. Вызов метода объекта [#166085]

---
+++ src/main/java/ru/job4j/opp/Student.java
@@ -0,0 +1,20 @@
+package ru.job4j.opp;
+
+public class Student {
+
+    public static void main(String[] args) {
+        Student petya = new Student();
+        for (int i = 0; i < 3; i++) {
+            petya.song();
+            petya.music();
+        }
+    }
+
+    public void music() {
+        System.out.println("Tra tra tra");
+    }
+
+    public void song() {
+        System.out.println("I believe I can fly");
+    }
+}
