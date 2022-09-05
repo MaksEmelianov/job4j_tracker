d9ff9ef25925d98035d10a5a1fca1cb2c105bf27
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-09-05 13:15:06 +0500

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,34 @@
+package ru.job4j.pojo;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book cleanCode = new Book("Clean code", 464);
+        Book philosophyJava = new Book("Philosophy java", 1168);
+        Book grokkingAlgorithms = new Book("Grokking algorithms", 288);
+        Book headJava = new Book("Head First Java", 688);
+        Book[] books = new Book[4];
+        books[0] = cleanCode;
+        books[1] = philosophyJava;
+        books[2] = grokkingAlgorithms;
+        books[3] = headJava;
+        for (Book bk : books) {
+            System.out.println(bk.getName() + " - " + bk.getCount() + " pages");
+        }
+        System.out.println();
+        System.out.println("Replace cleanCode to headJava.");
+        Book tmp = books[0];
+        books[0] = books[3];
+        books[3] = tmp;
+        for (Book bk : books) {
+            System.out.println(bk.getName() + " - " + bk.getCount() + " pages");
+        }
+        System.out.println();
+        System.out.println("Shown only book - \"Clean code\"");
+        for (Book bk : books) {
+            if (bk.getName().equals("Clean code")) {
+                System.out.println(bk.getName() + " - " + bk.getCount() + " pages");
+            }
+        }
+    }
+}
