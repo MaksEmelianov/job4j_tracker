223d4ae446837bc9c486915e07b9863de07559d2
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-09-05 16:53:53 +0500

    5. Tracker - хранилище [#396]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -5,12 +5,53 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class TrackerTest {
     @Test
-    public void whenAddNewItemThenTrackerHasSameItem() {
+    void whenAddNewItemThenTrackerHasSameItem() {
         Tracker tracker = new Tracker();
         Item item = new Item();
         item.setName("test1");
         tracker.add(item);
         Item result = tracker.findById(item.getId());
         assertThat(result.getName()).isEqualTo(item.getName());
+    }
+
+    @Test
+    void whenTestFindAll() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("Item1");
+        Item item2 = new Item("Item2");
+        tracker.add(item1);
+        tracker.add(item2);
+        Item result1 = tracker.findAll()[0];
+        Item result2 = tracker.findAll()[1];
+        assertThat(result1.getName()).isEqualTo(item1.getName());
+        assertThat(result2.getName()).isEqualTo(item2.getName());
+    }
+
+    @Test
+    void whenTestFindByNameCheckArrayLength() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("Item1");
+        Item item2 = new Item("Item2");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(new Item("Item1"));
+        tracker.add(new Item("Item2"));
+        tracker.add(new Item("Item1"));
+        Item[] result = tracker.findByName(item1.getName());
+        assertThat(result.length).isEqualTo(3);
+    }
+
+    @Test
+    void whenTestFindByNameCheckSecondNameItem() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("Item1");
+        Item item2 = new Item("Item2");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(new Item("Item1"));
+        tracker.add(new Item("Item2"));
+        tracker.add(new Item("Item1"));
+        Item[] result = tracker.findByName(item2.getName());
+        assertThat(result[0].getName()).isEqualTo(item2.getName());
     }
 }
