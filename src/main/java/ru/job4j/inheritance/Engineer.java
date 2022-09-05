433671377767fa8851184d171a7c717d472ae2e7
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-09-05 11:03:16 +0500

    3. Вызов конструктора родителя super(...) [#6837 #339677]

--- src/main/java/ru/job4j/inheritance/Engineer.java
+++ src/main/java/ru/job4j/inheritance/Engineer.java
@@ -1,9 +1,8 @@
 package ru.job4j.inheritance;
 
-public class Engineer extends Profession{
+public class Engineer extends Profession {
 
     private int experience;
-
 
     public Engineer(boolean degree, int experience) {
         super(degree);
