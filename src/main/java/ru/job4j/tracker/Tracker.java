9e6c061039595ecc65cd3a7efc8e339a6fe31993
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-09-06 16:56:31 +0500

    8. Что такое валидация? [#246864 #340083]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -36,11 +36,12 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        if (index != -1) {
+        boolean rsl = index != -1;
+        if (rsl) {
             item.setId(id);
             items[index] = item;
         }
-        return index != -1;
+        return rsl;
     }
 
     private int indexOf(int id) {
