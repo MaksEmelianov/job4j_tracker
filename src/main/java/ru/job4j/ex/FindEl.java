a8d609e425d96f97f4e139462d65e087d3ef98ff
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-09-08 20:19:36 +0500

    0.4. Пользовательские исключения. [#219367 #340971]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -7,12 +7,32 @@ public class FindEl {
         for (int index = 0; index < value.length; index++) {
             if (key.equals(value[index])) {
                 rsl = index;
+                break;
             }
         }
         if (rsl == -1) {
             throw new ElementNotFoundException("Данного элемента нет");
         }
         return rsl;
+    }
+
+    public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
+        for (String abuse : abuses) {
+            if (value.equals(abuse)) {
+                throw new ElementAbuseException("Есть запрещенный ключ");
+            }
+        }
+        return true;
+    }
+
+    public static void process(String[] value, String key, String[] abuses) {
+        try {
+            if (indexOf(value, key) != -1) {
+                sent(key, abuses);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
