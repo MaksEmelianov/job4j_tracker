2f6fb0bb01ca468e9586f8ad29e847b21dd0daad
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-09-10 20:06:02 +0500

    4. Принципы раннего возврата и охранных выражений [#504875 #341422]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -12,16 +12,16 @@ public class PasswordValidator {
         if (password.length() < 8 || password.length() > 32) {
             throw new IllegalArgumentException("Пароль несоответсвует шаблону длины: 8 - 32 символа");
         }
-        if (checkChars(password) == 0) {
+        if (!checkChars(password, 1)) {
             throw new IllegalArgumentException("Добавьте в пароль хотя бы одну строчную будку");
         }
-        if (checkChars(password) == 1) {
+        if (!checkChars(password, 2)) {
             throw new IllegalArgumentException("Добавьте в пароль хотя бы одну прописную будку");
         }
-        if (checkChars(password) == 2) {
+        if (!checkChars(password, 3)) {
             throw new IllegalArgumentException("Добавьте в пароль хотя бы одну цифру");
         }
-        if (checkChars(password) == 3) {
+        if (!checkChars(password, 4)) {
             throw new IllegalArgumentException("Добавьте в пароль хотя бы однин спец. знак");
         }
         return "Пароль валидный";
@@ -31,8 +31,8 @@ public class PasswordValidator {
         System.out.println(validate("admiiiiiiiiI1n"));
     }
 
-    private static int checkChars(String password) {
-        String msg = "";
+    /*
+    private static boolean checkChars(String password, int flag) {
         char[] chars = password.toCharArray();
         boolean[] flags = new boolean[4];
         for (char ch : chars) {
@@ -49,12 +49,39 @@ public class PasswordValidator {
                 flags[3] = true;
             }
         }
-        for (int index = 0; index < flags.length; index++) {
-            if (!flags[index]) {
-                return index;
+        return flags[flag];
+    }
+     */
+
+    private static boolean checkChars(String password, int flag) {
+        char[] chars = password.toCharArray();
+        for (char ch : chars) {
+            switch (flag) {
+                case 1:
+                    if (Character.isLowerCase(ch)) {
+                        return true;
+                    }
+                    break;
+                case 2:
+                    if (Character.isUpperCase(ch)) {
+                        return true;
+                    }
+                    break;
+                case 3:
+                    if (Character.isDigit(ch)) {
+                        return true;
+                    }
+                    break;
+                case 4:
+                    if (!Character.isLetterOrDigit(ch)) {
+                        return true;
+                    }
+                    break;
+                default:
+                    throw new IllegalStateException("Несуществующий фаг");
             }
         }
-        return -1;
+        return false;
     }
 
     private static boolean checkAbusePass(String password) {
