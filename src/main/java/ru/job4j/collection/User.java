a4590ca45868a2518f2098329b8c96bd5d0f7478
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-09-17 07:36:08 +0500

    1. Организовать сортировку User [#10034]

---
+++ src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,38 @@
+package ru.job4j.collection;
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public int compareTo(User o) {
+        int nameCom = name.compareTo(o.name);
+        int ageCom = Integer.compare(age, o.age);
+        return nameCom == 0 ? ageCom : nameCom;
+    }
+}
