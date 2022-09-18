7fd2caa14152ee95f91a896a352c9a992bba4099
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-09-18 11:18:21 +0500

    3. Компаратор для строк [#35008]

---
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class StringCompare implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        int rsl = 0;
+        int size = Math.min(o1.length(), o2.length());
+        for (int index = 0; index < size; index++) {
+            int compareChar = Character.compare(o1.charAt(index), o2.charAt(index));
+            if (compareChar != 0) {
+                rsl = Integer.compare(o1.charAt(index), o2.charAt(index));
+                break;
+            }
+        }
+        if (o1.length() != o2.length() && rsl == 0) {
+            rsl = Integer.compare(o1.length(), o2.length());
+        }
+        return rsl;
+    }
+}
---
+++ src/test/java/ru/job4j/collection/StringCompareTest.java
@@ -0,0 +1,60 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Disabled
+public class StringCompareTest {
+
+    @Test
+    void whenStringsAreEqualThenZero() {
+        StringCompare compare = new StringCompare();
+        int rsl = compare.compare(
+                "Ivanov",
+                "Ivanov"
+        );
+        assertThat(rsl).isEqualTo(0);
+    }
+
+    @Test
+    void whenLeftLessThanRightResultShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rsl = compare.compare(
+                "Ivanov",
+                "Ivanova"
+        );
+        assertThat(rsl).isLessThan(0);
+    }
+
+    @Test
+    void whenLeftGreaterThanRightResultShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rsl = compare.compare(
+                "Zaycev",
+                "Ivanova"
+        );
+        assertThat(rsl).isGreaterThan(0);
+    }
+
+    @Test
+    void charOfLeftGreaterThanRightShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rsl = compare.compare(
+                "Ivonova",
+                "Ivanova"
+        );
+        assertThat(rsl).isGreaterThan(0);
+    }
+
+    @Test
+    void charOfLeftLessThanRightShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int rsl = compare.compare(
+                "Ivanova",
+                "Ivonova"
+        );
+        assertThat(rsl).isLessThan(0);
+    }
+}
