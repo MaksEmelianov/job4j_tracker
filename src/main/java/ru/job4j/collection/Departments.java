7c621e13a841b50beb4a69340e7b2ef947298a07
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-09-20 09:30:00 +0500

    2. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+
+    public static List<String> fillGaps(List<String> deps) {
+        Set<String> tmp = new LinkedHashSet<>();
+        for (String value : deps) {
+            String tmpStr = "";
+            for (String el : value.split("/")) {
+                tmpStr = tmpStr.isEmpty() ? el : tmpStr + "/" + el;
+                tmp.add(tmpStr);
+            }
+        }
+        return new ArrayList<>(tmp);
+    }
+
+    public static void sortAsc(List<String> args) {
+        args.sort(Comparator.naturalOrder());
+    }
+
+    public static void sortDesc(List<String> args) {
+        args.sort(new DepDescComp().thenComparing(Comparator.naturalOrder()));
+    }
+}
---
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -0,0 +1,153 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DepartmentsTest {
+
+    @Test
+    void whenMissed() {
+        List<String> in = Arrays.asList("k1/sk1", "k2/sk1");
+        List<String> expected = Arrays.asList("k1", "k1/sk1", "k2", "k2/sk1");
+        List<String> out = Departments.fillGaps(in);
+        assertThat(out).containsAll(expected);
+    }
+
+    @Test
+    void whenMissed2() {
+        List<String> in = Arrays.asList(
+                "K1/SK1",
+                "K1/SK2",
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K2",
+                "K2/SK1/SSK1",
+                "K2/SK1/SSK2");
+        List<String> expected = Arrays.asList(
+                "K1",
+                "K1/SK1",
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K1/SK2",
+                "K2",
+                "K2/SK1",
+                "K2/SK1/SSK1",
+                "K2/SK1/SSK2");
+        List<String> out = Departments.fillGaps(in);
+        assertThat(out).containsAll(expected);
+    }
+
+    @Test
+    void whenNonChange() {
+        List<String> input = Arrays.asList("k1", "k1/sk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> result = Departments.fillGaps(input);
+        assertThat(result).containsAll(expect);
+    }
+
+    @Test
+    void whenSortAscWithoutMissedDepartments() {
+        List<String> in = Arrays.asList(
+                "K1/SK1",
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K2",
+                "K2/SK1",
+                "K1/SK2",
+                "K1",
+                "K2/SK1/SSK2",
+                "K2/SK1/SSK1"
+        );
+        List<String> expected = Arrays.asList(
+                "K1",
+                "K1/SK1",
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K1/SK2",
+                "K2",
+                "K2/SK1",
+                "K2/SK1/SSK1",
+                "K2/SK1/SSK2"
+        );
+        Departments.sortAsc(in);
+        assertThat(in).containsAll(expected);
+    }
+
+    @Test
+    void whenSortAscWithMissedDepartments() {
+        List<String> in = Arrays.asList(
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K2/SK1",
+                "K1/SK2",
+                "K2/SK1/SSK2",
+                "K2/SK1/SSK1"
+        );
+        List<String> expect = Arrays.asList(
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K1/SK2",
+                "K2/SK1",
+                "K2/SK1/SSK1",
+                "K2/SK1/SSK2"
+        );
+        Departments.sortAsc(in);
+        assertThat(in).containsAll(expect);
+    }
+
+    @Test
+    void whenSortDescWithoutMissedDepartments() {
+        List<String> in = Arrays.asList(
+                "K1/SK1",
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K2",
+                "K2/SK1",
+                "K1/SK2",
+                "K1",
+                "K2/SK1/SSK2",
+                "K2/SK1/SSK1"
+        );
+        List<String> expect = Arrays.asList(
+                "K2",
+                "K2/SK1",
+                "K2/SK1/SSK1",
+                "K2/SK1/SSK2",
+                "K1",
+                "K1/SK1",
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K1/SK2"
+        );
+        Departments.sortDesc(in);
+        assertThat(in).containsAll(expect);
+    }
+
+    @Test
+    void whenSortDescWithMissedDepartments() {
+        List<String> in = Arrays.asList(
+                "K1/SK1",
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K2/SK1",
+                "K1/SK2",
+                "K2/SK1/SSK2",
+                "K2/SK1/SSK1"
+        );
+        List<String> expect = Arrays.asList(
+                "K2/SK1",
+                "K2/SK1/SSK1",
+                "K2/SK1/SSK2",
+                "K1/SK1",
+                "K1/SK1/SSK1",
+                "K1/SK1/SSK2",
+                "K1/SK2"
+        );
+        Departments.sortDesc(in);
+        assertThat(in).containsAll(expect);
+    }
+}
