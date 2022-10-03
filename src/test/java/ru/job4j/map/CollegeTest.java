8ab6b1f00eb5285414f2aa0ae77f18e3f0dd6579
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-10-03 10:38:14 +0500

    3. Optional - empty, of [#214711]

---
+++ src/test/java/ru/job4j/map/CollegeTest.java
@@ -0,0 +1,107 @@
+package ru.job4j.map;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CollegeTest {
+
+    @Test
+    void whenAccountIsOptionalEmpty() {
+        Map<Student, Set<Subject>> students = Map.of(
+                new Student("Student1", "000001", "201-18-15"),
+                Set.of(
+                        new Subject("Math", 70),
+                        new Subject("English", 85)
+                ),
+                new Student("Student2", "000002", "201-18-15"),
+                Set.of(
+                        new Subject("Economic", 75),
+                        new Subject("Sociology", 65)
+                )
+        );
+        College college = new College(students);
+        assertThat(college.findByAccount("0001"))
+                .isEqualTo(Optional.empty());
+    }
+
+    @Test
+    void whenAccountIsOptionalNotEmpty() {
+        Map<Student, Set<Subject>> students = Map.of(
+                new Student("Student1", "000001", "201-18-15"),
+                Set.of(
+                        new Subject("Math", 70),
+                        new Subject("English", 85)
+                ),
+                new Student("Student2", "000002", "201-18-15"),
+                Set.of(
+                        new Subject("Economic", 75),
+                        new Subject("Sociology", 65)
+                )
+        );
+        College college = new College(students);
+        assertThat(college.findByAccount("000001").get().getNameStudent())
+                .isEqualTo("Student1");
+    }
+
+    @Test
+    void whenSubjectIsOptionalEmptyWithNotFoundAccount() {
+        Map<Student, Set<Subject>> students = Map.of(
+                new Student("Student1", "000001", "201-18-15"),
+                Set.of(
+                        new Subject("Math", 70),
+                        new Subject("English", 85)
+                ),
+                new Student("Student2", "000002", "201-18-15"),
+                Set.of(
+                        new Subject("Economic", 75),
+                        new Subject("Sociology", 65)
+                )
+        );
+        College college = new College(students);
+        assertThat(college.findBySubjectName("001", "Math"))
+                .isEqualTo(Optional.empty());
+    }
+
+    @Test
+    void whenSubjectIsOptionalEmptyWithFoundAccount() {
+        Map<Student, Set<Subject>> students = Map.of(
+                new Student("Student1", "000001", "201-18-15"),
+                Set.of(
+                        new Subject("Math", 70),
+                        new Subject("English", 85)
+                ),
+                new Student("Student2", "000002", "201-18-15"),
+                Set.of(
+                        new Subject("Economic", 75),
+                        new Subject("Sociology", 65)
+                )
+        );
+        College college = new College(students);
+        assertThat(college.findBySubjectName("000001", "Economic"))
+                .isEqualTo(Optional.empty());
+    }
+
+    @Test
+    void whenSubjectIsOptionalNotEmpty() {
+        Map<Student, Set<Subject>> students = Map.of(
+                new Student("Student1", "000001", "201-18-15"),
+                Set.of(
+                        new Subject("Math", 70),
+                        new Subject("English", 85)
+                ),
+                new Student("Student2", "000002", "201-18-15"),
+                Set.of(
+                        new Subject("Economic", 75),
+                        new Subject("Sociology", 65)
+                )
+        );
+        College college = new College(students);
+        assertThat(college.findBySubjectName("000001", "Math").get().getScore())
+                .isEqualTo(70);
+    }
+}
